bc28bc1fdccce496a0084e709e945276a6643a41
Ray <dan@raydevs.com>
2025-03-02 11:54:20 +0900

    enhance Header component with new background styles and improve layout structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,13 @@ import { ThemeToggleButton } from "./ThemeToggleButton";
 import { LanguageSetting } from "../Button/LanguageSetting.tsx";
 import React from "react";
 import { ContentBar } from "../ContentBar.tsx";
+import "./Header.css";
 
 export const Header: React.FC = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 bg-light dark:bg-dark-900">
+    <div className="header-bg fixed top-0 left-0 right-0 z-50">
       <ContentBar />
-      <div className="w-full  mx-auto px-4 flex items-center justify-between h-16">
+      <div className="footer w-full  mx-auto px-4 flex items-center justify-between h-16">
         <div
           onClick={() => (window.location.href = "/")}
           className="raydevs font-led font-bold text-2xl cursor-pointer text-t-dark dark:text-t-light "
