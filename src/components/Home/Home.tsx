98dfe1f6526aaa5686fb994d98a40b90579b80aa
Ray <dan@raydevs.com>
2025-03-11 14:43:15 +0900

    xyz

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,7 +3,7 @@ import { Profile } from "../Profile/Profile";
 import { Core } from "../Core/Core.tsx";
 import { Sections } from "../../enum.ts";
 import { useNavStore } from "../../store.ts";
-import { HorizontalScrollCarousel, Resume } from "../Resume/Resume.tsx";
+import { Resume } from "../Resume/Resume.tsx";
 
 interface HomeProps {
   setFloatingTop: React.Dispatch<React.SetStateAction<number>>;
